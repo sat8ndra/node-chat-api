61b70cab0806746a2611309418dcb241e291ee56
satyendra gupta <sat8ndra@gmail.com>
2019-01-23 01:19:47 +0530

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'abc@123.com',
+    text: 'test email to view on browser window',
+    createdAt: 123123
+  });
+
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log("User was disonnected");
   });
