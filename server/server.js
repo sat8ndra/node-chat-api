00be869021773bbf6db93b888fa81713638a5775
satyendra gupta <sat8ndra@gmail.com>
2019-01-24 01:38:55 +0530

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // socket.emit from Admin text Welcome to the chat app
+
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app'
+  });
+
+  // socket.broadcast.emit from Admin text New user joined
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
@@ -23,6 +37,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
